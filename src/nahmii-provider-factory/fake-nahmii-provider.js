dcc4717008dcf30ee2897ef25555d027018edd89
Erik Baklund <erik@hubii.com>
2019-08-07 20:19:48 +0200

    Fix unittest usning provider.once()

--- src/nahmii-provider-factory/fake-nahmii-provider.js
+++ src/nahmii-provider-factory/fake-nahmii-provider.js
@@ -1,27 +1,24 @@
 'use strict';
 
+const EventEmitter = require('events');
+
 const sinon = require('sinon');
 
 class FakeNahmiiProvider {
   constructor () {
+    this.eventEmitter = new EventEmitter();
     this.reset();
   }
 
   reset () {
-    this.onceStub = sinon.stub().returns(0);
     this.getBlockNumber = sinon.stub().returns(0);
     this.getLogs = sinon.stub().throws(new Error('getLogs() stub is not initialized.'));
     this.getBlock = sinon.stub().throws(new Error('getBlock() stub is not initialized.'));
+    this.eventEmitter.removeAllListeners();
   }
 
-  async once (tag, cb) {
-    await new Promise(resolve => setTimeout(resolve, 100)); // Avoid hogging CPU
-    return this.onceStub(tag, cb);
-  }
-
-  setBlockNumber (blockNo) {
-    this.onceStub.callsArgWith(1, blockNo);
-    this.getBlockNumber.returns(blockNo);
+  async once (eventName, eventListener) {
+    this.eventEmitter.once(eventName, eventListener);
   }
 }
 
